c3c9b0bebc7d31af48cdb2e500a9fc4c2d79d256
sociosofia <luizvjj@gmail.com>
2026-08-10 09:38:25 -0300

    v0.8: atualiza cache do PWA

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'adelita-pwa-v0.7.1';
+const VERSION = 'adelita-pwa-v0.8';
 const SHELL = [
   './', './index.html', './manifest.webmanifest', './METADADOS_BANCOS.json',
   './assets/logo_etec_bayeux.png', './assets/logo_bayeux.png', './assets/logo_cps.png',
