31f52e7128b7333b419df1801aafe84ea8e239ef
sociosofia <luizvjj@gmail.com>
2026-08-11 08:56:04 -0300

    v1.0.1: atualiza cache do DOCX refinado

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'adelita-pwa-v1.0';
+const VERSION = 'adelita-pwa-v1.0.1';
 const SHELL = [
   './', './index.html', './manifest.webmanifest', './METADADOS_BANCOS.json', './builder_v1.js',
   './assets/logo_etec_bayeux.png', './assets/logo_bayeux.png', './assets/logo_cps.png',
